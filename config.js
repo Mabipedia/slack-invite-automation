cf5302f15438153d9764e53b9051f757a5721cc9
cristears <cristears@users.noreply.github.com>
2015-07-02 12:44:25 +0900

    Update config.js with settings for Mabipedia
    마비피디아에 대한 Slack 정보 추가

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   // your community or team name to display on join page.
-  community: process.env.COMMUNITY_NAME || 'YOUR-TEAM-NAME',
+  community: process.env.COMMUNITY_NAME || '마비피디아',
   // your slack team url (ex: socketio.slack.com)
-  slackUrl: process.env.SLACK_URL || 'YOUR-TEAM.slack.com',
+  slackUrl: process.env.SLACK_URL || 'mabipedia.slack.com',
   // access token of slack
   // You can generate it in https://api.slack.com/web#auth
   //
@@ -10,5 +10,5 @@ module.exports = {
   //   curl -X POST 'https://YOUR-SLACK-TEAM.slack.com/api/users.admin.invite' \
   //   --data 'email=EMAIL&token=TOKEN&set_active=true' \
   //   --compressed
-  slacktoken: process.env.SLACK_TOKEN || 'YOUR-ACCESS-TOKEN'
+  slacktoken: process.env.SLACK_TOKEN || 'xoxp-3119751786-3119751790-7134024723-49edd0'
 };
